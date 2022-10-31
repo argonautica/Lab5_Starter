fdaf79ac0dd5062a03fb23175b49c9208bf18ed9
argonautica <sidplusk@yahoo.com>
2022-10-30 21:28:26 -0700

    explored

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,37 @@
 // explore.js
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+    const face = document.getElementsByTagName('img')[0];
+    const voiceOptions = document.getElementsByTagName('select')[0];
+    const button = document.getElementsByTagName('button')[0];
+    const textInput = document.getElementById("text-to-speak");
+    const synth = window.speechSynthesis;
+    let voices = [];
+    synth.addEventListener('voiceschanged', () => {
+        voices = synth.getVoices();
+        for (let i = 0; i < voices.length; i++) {
+            const option = document.createElement('option');
+            option.textContent = `${voices[i].name}`;
+            option.setAttribute('data-name', voices[i].name);
+            voiceOptions.appendChild(option);
+        }
+    });
+    button.addEventListener('click', () => {
+        face.src = "assets/images/smiling-open.png";
+        let utterThis = new SpeechSynthesisUtterance(textInput.value);
+        let selectedOption = voiceOptions.selectedOptions[0].getAttribute('data-name');
+        for (let i = 0; i < voices.length; i++) {
+            if (voices[i].name === selectedOption) {
+                utterThis.voice = voices[i];
+            }
+        }
+        synth.speak(utterThis);
+        setInterval(function() {
+            if (!synth.speaking) {
+                face.src = "assets/images/smiling.png";
+                clearInterval();
+            }
+        }, 100)
+    });
 }
